5052ce4c0b46fb1a326b4258f8d7feb4eb499321
layne <layneshi0120@hotmail.com>
2020-05-27 23:09:43 +0800

    1.9.0

---
+++ Frameworks/biwenSDK.framework/Headers/biwenSDK.h
@@ -0,0 +1,20 @@
+//
+//  biwenSDK.h
+//  biwenSDK
+//
+//  Created by layne on 2020/4/30.
+//  Copyright © 2020 layne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for biwenSDK.
+FOUNDATION_EXPORT double biwenSDKVersionNumber;
+
+//! Project version string for biwenSDK.
+FOUNDATION_EXPORT const unsigned char biwenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <biwenSDK/PublicHeader.h>
+
+#import <biwenSDK/NoseInterface.h>
+#import <biwenSDK/NoseErrorDefine.h>
