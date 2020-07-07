e27d197a07b870b4ee23bf891853a8fb7149fe7b
layne <layneshi0120@hotmail.com>
2020-07-07 10:13:51 +0800

    2.3.0

---
+++ Frameworks/biwenSDK.framework/Headers/NoseCollectModel.h
@@ -0,0 +1,26 @@
+//
+//  NoseCollectModel.h
+//  TestDemo
+//
+//  Created by layne on 2020/5/24.
+//  Copyright © 2020 layne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NoseCollectModel : NSObject
+
+@property (nonatomic, strong) NSString *age;
+@property (nonatomic, strong) NSString *code;
+@property (nonatomic, strong) NSString *createTime;
+@property (nonatomic, strong) NSString *headImg;
+@property (nonatomic, strong) NSString *ID;
+@property (nonatomic, strong) NSString *rate;
+@property (nonatomic, assign) BOOL same;
+@property (nonatomic, strong) NSString *type;
+
+@end
+
+NS_ASSUME_NONNULL_END
